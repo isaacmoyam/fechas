2288572628707517277a4c2786af6daf0dfcbbdb
isaacmoyam <146216220+isaacmoyam@users.noreply.github.com>
2023-10-18 13:32:58 +0200

    Add files via upload

--- fechas.php
+++ fechas.php
@@ -1,48 +1,12 @@
 <!--ISAAC MOYA-->
 <?php
-	class Fecha {
-		private $meses = array(
-			1 => array('Enero', 31),
-			2 => array('Febrero', 28),
-			3 => array('Marzo', 31),
-			4 => array('Abril', 30),
-			5 => array('Mayo', 31),
-			6 => array('Junio', 30),
-			7 => array('Julio', 31),
-			8 => array('Agosto', 31),
-			9 => array('Septiembre', 30),
-			10 => array('Octubre', 31),
-			11 => array('Noviembre', 30),
-			12 => array('Diciembre', 31)
-		);
-
-		public function mostrarFecha($fecha) {
-			$fechaPartes = explode('-', $fecha); 
-			$anio = $fechaPartes[0];
-			$mes = $fechaPartes[1];
-			$dia = $fechaPartes[2];
-
-			if ($this->esBisiesto($anio)) {
-				$this->meses[2][1] = 29;
-			}
-
-			$nombreMes = $this->meses[(int)$mes][0];
-			$diasDelMes = $this->meses[(int)$mes][1];
-
-			echo $dia.' / '.$nombreMes.' / '.$anio.'<br>';
-			echo "$nombreMes tiene $diasDelMes días.";
-		}
-
-		private function esBisiesto($anio) {
-			return ($anio % 4 == 0 && $anio % 100 != 0) || ($anio % 400 == 0);
-		}
-	}
-
-	if (!empty($_POST["fecha"])) {
+	include 'clase.php';
+	if(!empty($_POST["fecha"])){
 		$fecha = $_POST["fecha"];
 		$fechaObj = new Fecha();
-		$fechaObj->mostrarFecha($fecha);
-	} else {
+		$resultado = $fechaObj->mostrarFecha($fecha);
+		echo $resultado;
+	}else{
 		echo "Has mandado la fecha vacía";
 	}
 ?>
